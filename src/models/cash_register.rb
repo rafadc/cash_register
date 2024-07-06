2ae813aed627beedae1dc918528cd47c35c40a18
Rafa de Castro <rafael@micubiculo.com>
2024-07-06 10:04:27 +0200

    Accepting different kinds of products

--- src/models/cash_register.rb
+++ src/models/cash_register.rb
@@ -6,14 +6,15 @@ class CashRegister
     @items = Hash.new(0)
   end
 
-  def add(item, quantity)
-    @items[item] += quantity
+  def add(product_id, quantity)
+    @items[product_id] += quantity
   end
 
   def total
     @total = 0.0
-    @items.each do |_item, quantity|
-      @total += quantity
+    @items.each do |product_id, quantity|
+      price = ProductRepository.new.find(product_id).price
+      @total += quantity * price
     end
     @total
   end
---
+++ src/models/product_repository.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Repository class to retrieve products
+# At this moment this will work just with an in memory array of products
+class ProductRepository
+  PRODUCTS = [
+    Product.new('GR1', 'Green Tea', 3.11),
+    Product.new('SR1', 'Strawberries', 5.00),
+    Product.new('CF1', 'Coffee', 11.23)
+  ].freeze
+
+  def find(id)
+    PRODUCTS.find { |product| product.id == id }
+  end
+end

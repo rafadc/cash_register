3c027a9a28b372c75ae2fe63d9ca2ea946a16dd8
Rafa de Castro <rafael@micubiculo.com>
2024-07-06 23:52:25 +0200

    Moved discounts to config file
    Signed-off-by: Rafa de Castro <rafael@micubiculo.com>

--- src/models/discounts/discount_repository.rb
+++ src/models/discounts/discount_repository.rb
@@ -3,14 +3,28 @@
 module Discounts
   # The repository to retrieve the discounts we have configured
   class DiscountRepository
-    DISCOUNTS = [
-      BuyOneGetOneFree.new('GR1'),
-      BulkDiscountFixedPrice.new('SR1', 3, 4.50),
-      BulkDiscountPercentage.new('CF1', 3, 33)
-    ].freeze
-
     def all
-      DISCOUNTS
+      discounts
+    end
+
+    private
+
+    def discounts
+      @discounts ||= read_discounts_from_config
+    end
+
+    def read_discounts_from_config
+      @discounts = YAML.load_file('src/configuration.yml')['discounts']
+      @discounts.map do |discount|
+        case discount['type']
+        when 'buy-one-get-one-free'
+          BuyOneGetOneFree.new(discount['code'])
+        when 'bulk-discount-fixed-price'
+          BulkDiscountFixedPrice.new(discount['code'], discount['min_quantity'], discount['discount_price'])
+        when 'bulk-discount-percentage'
+          BulkDiscountPercentage.new(discount['code'], discount['min_quantity'], discount['discount_percentage'])
+        end
+      end
     end
   end
 end
